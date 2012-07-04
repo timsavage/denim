9b0538ba3bdaf65a82b2a27173c1579bb83eea4d
Tim Savage <tim.savage@poweredbypenguins.org>
2012-07-04 18:03:28 +1000

    Improved to comply with pep8, updated documentation

--- denim/virtualenv.py
+++ denim/virtualenv.py
@@ -1,3 +1,4 @@
+# -*- encoding:utf8 -*-
 from fabric.api import prefix
 import utils
 import paths
@@ -9,20 +10,21 @@ def create(path=None, use_sudo=True, user=None):
     """
     Create a virtual environment.
 
-    :path: location to create virtual env; default is deploy path.
-    :use_sudo: run this command with sudo; default is True.
-    :user: when using sudo run as this user; default is None (or root).
+    :param path: location to create virtual env; default is deploy path.
+    :param use_sudo: run this command with sudo; default is True.
+    :param user: when using sudo run as this user; default is None (or root).
 
     """
     if not path:
         path = paths.deploy_path()
     utils.run_as('virtualenv %s' % path, use_sudo=use_sudo, user=user)
 
+
 def activate(path=None):
     """
     Context manager to enable a virtual env.
 
-    :path: location of the virtual env to activate; default is deploy path.
+    :param path: location of the virtual env to activate; default is deploy path.
 
     """
     if not path:
