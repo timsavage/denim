2c91820216aee856032f2fb429b0c171b460b6c3
tim.savage@poweredbypenguins.org <tim.savage@poweredbypenguins.org>
2012-07-05 01:05:29 +1000

    Added archive and tag support for git and hg

--- denim/scm/__init__.py
+++ denim/scm/__init__.py
@@ -1,17 +1,53 @@
 # -*- encoding:utf8 -*-
+from fabric.api import env, task, require
 from denim._env_proxy import Proxy
+from denim import utils
+from denim import paths
 
 
 __proxy = Proxy('deploy_scm', globals(), 'hg')
 
-tag_release = __proxy.method('tag_release', True, doc=
+tag = __proxy.method('tag', False, doc=
 """
-Add a tag to a GIT repository for a release.
+Add a tag to the local repository.
+""")
 
-Release tags have the following format release-%Y.%m.%d.revision
-
-_usage_: fab scm.tag_release:1
+archive = __proxy.method('archive', False, doc=
 """
-)
+Archive from local repository.
+""")
+
+
+@task
+@__proxy.local_method
+def tag_release(increment='0'):
+    """
+    Tag release in repository.
+
+    Release tags have the following format release-%Y.%m.%d.increment
+
+    :param increment: Incremental release number.
+    :return: The created revision
+    """
+    version = utils.generate_version(increment)
+    tag(version)
+    print('Tagged version:\t{0}\nRelease tag:\trelease-{0}'.format(version))
+    return version
+
+
+@__proxy.local_method
+def archive_app(revision, out_file=None):
+    """
+    Create an archive or the app folder for deployment.
+
+    :param revision: Revision to deploy.
+    :param out_file: Optional name of the archive file.
+    :return: path of generated file.
+    """
+    if not out_file:
+        require('package_name')
+        out_file = '%(package_name)s.tar.gz' % env
+    archive(revision, out_file, 'app')
+    return out_file
 
 __all__ = __proxy.methods
--- denim/scm/hg.py
+++ denim/scm/hg.py
@@ -1,1 +1,16 @@
 # -*- encoding:utf8 -*-
+from fabric.api import local
+
+
+def tag(version):
+    local('hg tag -m "Release {0}" release-{0}'.format(version))
+
+
+def archive(revision, out_file, sub_path, prefix=None):
+    args = [
+        '-r ' + revision,
+        '-I ' + sub_path
+    ]
+    if prefix:
+        args.append('-p ' + prefix)
+    local('hg archive %s %s' % (' '.join(args), out_file))
