d902637824d83dc312d23a73a564aa8d820f2e90
tim.savage@poweredbypenguins.org <tim.savage@poweredbypenguins.org>
2012-07-05 01:22:45 +1000

    Added archive_and_upload method

---
+++ denim/deploy.py
@@ -0,0 +1,14 @@
+# -*- encoding:utf8 -*-
+from fabric.api import put
+from denim import scm, paths, utils
+
+
+def archive_and_upload(revision, *args, **kwargs):
+    """
+    Upload application archive based on a source control revision.
+    :param revision: revision to deploy.
+    """
+    archive_file = scm.archive_app(revision)
+    put(archive_file, '/tmp/%s' % archive_file)
+    with paths.cd('app'):
+        utils.run_as('tar -xzf /tmp/%s' % archive_file, *args, **kwargs)
