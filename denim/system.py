a4fb65983802dbf5c0ded5c8089760de91a93100
Tim Savage <tim.savage@poweredbypenguins.org>
2012-07-04 18:11:34 +1000

    Improved to comply with pep8, updated documentation, added remove_file command (basically rm but with some catches and optional runas)

--- denim/system.py
+++ denim/system.py
@@ -1,16 +1,18 @@
+# -*- encoding:utf8 -*-
 from fabric.api import env, sudo, run, settings, hide
 from fabric.contrib import files
+from denim import utils
 
 
 def user_exists(user=None):
     """
     Check if a user exists.
 
-    :user: name of the user to check; defaults to the deploy_user.
+    :param user: name of the user to check; defaults to the deploy_user.
 
     """
     if not user:
-        user_name = env.deploy_user
+        user = env.deploy_user
 
     with settings(
         hide('warnings', 'running', 'stdout', 'stderr'),
@@ -20,20 +22,19 @@ def user_exists(user=None):
 
     return result.return_code == 0
 
-def create_system_user(user=None, home=None):
+
+def create_system_user(user=env.deploy_user, home=None):
     """
     Create a system user.
 
     If user already exists will ignore the operation.
 
-    :user: name of the user to create; defaults to the deploy_user.
-    :home: path to home directory of user; defaults to deploy_path.
+    :param user: name of the user to create; defaults to the deploy_user.
+    :param home: path to home directory of user; defaults to deploy_path.
 
-    :returns: True if user is created; else False to indicate user already exists.
+    :return: True if user is created; else False to indicate user already
+        exists.
     """
-    if not user:
-        user = env.deploy_user
-
     if not user_exists(user):
         sudo('adduser --system --quiet --home %(home)s %(user)s' % {
             'home': home if home else env.deploy_path,
@@ -43,33 +44,43 @@ def create_system_user(user=None, home=None):
     else:
         return False
 
-def change_owner(path, recursive=False, user=None):
+
+def change_owner(path, recursive=False, user=env.deploy_user):
     """
     Change the owner of a path.
 
-    :path: to change owner of.
-    :recursive: if the path references a folder recurs through all sub folders.
-    :user: name of the user to make owner; defaults to the deploy_user.
+    :param path: to change owner of.
+    :param recursive: if the path references a folder recurs through all sub
+        folders.
+    :param user: name of the user to make owner; defaults to the deploy_user.
     """
-    if not user:
-        user = env.deploy_user
+    sudo('chown %s %s. %s' % ('-R' if recursive else '', user, path))
 
-    sudo('chown %s %s. %s' % (
-        '-R' if recursive else '',
-        user,
-        path
-    ))
 
-def create_symlink(target_path, link_path, replace_existing=True, use_sudo=False):
+def create_symlink(target_path, link_path, replace_existing=True,
+                   *args, **kwargs):
     """
     Create a symlink on remote server.
 
-    :target_path: target of symlink.
-    :link_path: location of symlink.
-    :replace_existing: overwrite an existing symlink; else fail if link already exists.
-    :use_sudo: perform operation with sudo.
+    :param target_path: target of symlink.
+    :param link_path: location of symlink.
+    :param replace_existing: overwrite an existing symlink; else fail if link
+        already exists.
     """
-    exe = sudo if use_sudo else run
-    if replace_existing and files.exists(link_path):
-        exe('rm "%s"' % link_path)
-    exe('ln -s "%s" "%s"' % (target_path, link_path))
+    if replace_existing:
+        remove_file(link_path, *args, **kwargs)
+    utils.run_as('ln -s "%s" "%s"' % (target_path, link_path), *args, **kwargs)
+
+
+def remove_file(path, *args, **kwargs):
+    """
+    Remove a file from remote server.
+
+    :param path: to file.
+    :return: True if file existed and was removed else False
+
+    """
+    if not files.exists(path):
+        return False
+    utils.run_as('rm "%s"' % path, *args, **kwargs)
+    return True
