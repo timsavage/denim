65d6548598ca6337e654291496ea84df434bea6a
Tim Savage <tim.savage@poweredbypenguins.org>
2012-07-04 18:01:51 +1000

    Improved to comply with pep8, updated documentation

--- denim/utils.py
+++ denim/utils.py
@@ -1,32 +1,36 @@
+# -*- encoding:utf8 -*-
 from datetime import date
 from fabric.api import run, sudo
 
-__all__ = ('run_as', )
+__all__ = ('run_as', 'generate_version')
 
 
 def run_as(command, use_sudo=False, user=None, **kwargs):
     """
     A wrapper around run and sudo that allows a user to be provided.
 
-    :command: command to run as.
-    :use_sudo: run this command with sudo; default is False.
-    :user: if using sudo run command as this user; default None (root).
+    :param command: command to run as.
+    :param use_sudo: run this command with sudo; default is False.
+    :param user: if using sudo run command as this user; default None (root).
+
     """
     if use_sudo:
         return sudo(command, user=user, **kwargs)
     else:
         return run(command, **kwargs)
 
+
 def generate_version(revision=None):
     """
     Generate a version number based on today's date and an optional revision.
-    
-    Version string is in the format %Y.%m.%d.revision
+
+    Version string is in the format %Y.%m.%d.revision.
+
     """
     version = date.today().strftime('%Y.%m.%d')
     if revision:
         try:
             version += '.{0}'.format(int(revision))
         except ValueError:
-            raise ValueError("Revision must be an integer value.")  
+            raise ValueError("Revision must be an integer value.")
     return version
