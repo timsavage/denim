e40589eae5ec2a6e4dd2e44d4058902c7b36f58e
Tim Savage <tim.savage@poweredbypenguins.org>
2012-07-06 14:39:17 +1000

    Improved documentation, move deploy commands into recipes, added support for testing for/installing OS packages

--- denim/recipes.py
+++ denim/recipes.py
@@ -29,8 +29,34 @@ Django specific items
 - Symlink new release as current
 
 """
-from fabric.api import sudo, require
-from denim import deploy, paths, pip, system, virtualenv, webserver
+from fabric import colors
+from fabric.api import abort, put, require, sudo
+from denim import (deploy, package, paths, pip, scm, system, utils, virtualenv,
+                   webserver)
+
+
+# These are the default packages for Nginx, PostgreSQL and Supervisor for
+# Debian GNU/Linux or derivatives.
+DEFAULT_PACKAGES = [
+    'nginx', # Web server
+    'supervisor', # Process Manager
+
+    # Python requirements
+    'python-virtualenv'
+]
+
+
+def archive_and_upload(revision, use_sudo=True, user=None):
+    """
+    Upload application archive based on a source control revision.
+
+    :param revision: revision to deploy.
+
+    """
+    archive_file = scm.archive_app(revision)
+    put(archive_file, '/tmp/%s' % archive_file)
+    with paths.cd('app'):
+        utils.run_as('tar -xzf /tmp/%s' % archive_file, use_sudo, user)
 
 
 def create_standard_layout():
@@ -51,20 +77,36 @@ def create_standard_layout():
     system.change_owner(log_path)
 
 
-def standard_provision():
+def standard_provision(install_packages=True,
+                       required_packages=DEFAULT_PACKAGES):
     """
     Standard provisioning recipe.
+
+    :param install_packages: indicates if packages should be installed if they
+        are not on the server.
+    :param required_packages: list of packages that are required on the server.
+
+    If `install_packages` is `False` and the package is missing provisioning
+    will abort.
     """
     require('project_name', 'package_name', 'deploy_env')
 
-    # Layout
+    print colors.yellow("* Check correct packages are installed.")
+    for package_name in required_packages:
+        if not package.is_installed(package_name):
+            if install_packages:
+                package.install(package_name)
+            else:
+                abort('* Package "%s" is not available on the server.' % package_name)
+
+    print colors.yellow("* Create standard layout.")
     system.create_system_user()
     create_standard_layout()
     virtualenv.create()
 
-    # Configuration
+    print colors.yellow("* Upload configuration for dependant services.")
+    print colors.yellow("** Web server.")
     webserver.install_config()
-
 
 
 def standard_deploy(revision):
@@ -75,7 +117,7 @@ def standard_deploy(revision):
 
 
 
-    deploy.archive_and_upload(revision)
+    archive_and_upload(revision)
     with virtualenv.activate():
         pip.install_requirements(revision, use_sudo=True)
 
