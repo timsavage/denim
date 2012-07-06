93004d401bb9907426687f50b26484b0a02a6fa5
tim.savage@poweredbypenguins.org <tim.savage@poweredbypenguins.org>
2012-07-06 21:45:54 +1000

    Added nosetests requirement and setup a test command in fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,1 +1,12 @@
-from denim import *
+from fabric.api import *
+
+
+@task
+def test(with_coverage=False):
+    """
+    Run denim unit tests. Pass True to enable coverage.
+    """
+    if with_coverage:
+        local('nosetests -w tests --all-modules --with-coverage --cover-package denim')
+    else:
+        local('nosetests -w tests --all-modules')
