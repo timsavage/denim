676f1714ab126b51abcbcf4d0f0846133785712b
Tim Savage <tim.savage@poweredbypenguins.org>
2012-07-06 16:45:40 +1000

    Added all details for supervisor

--- denim/service/__init__.py
+++ denim/service/__init__.py
@@ -22,6 +22,31 @@ Upload configuration file.
 
 :param name_prefix: Prefix to append to service name to provide alternate
     configuration (or support multiple services)
+
+""")
+
+manager_start = __proxy.method('manager_start', task=True, doc=
+"""
+Start service manager.
+
+""")
+
+manager_stop = __proxy.method('manager_stop', task=True, doc=
+"""
+Stop service manager.
+
+""")
+
+manager_restart = __proxy.method('manager_restart', task=True, doc=
+"""
+Restart service manager.
+
+""")
+
+manager_reload = __proxy.method('manager_reload', task=True, doc=
+"""
+Reload service manager.
+
 """)
 
 
@@ -31,24 +56,42 @@ start = __proxy.method('start', True, doc=
 """
 Start process.
 
-:param name_prefix: Prefix to append to service name to provide alternate
-    configuration (or support multiple services)
+:param service_name: name of the service to start.
+
 """)
 
 stop = __proxy.method('stop', True, doc=
 """
 Stop process.
 
-:param name_prefix: Prefix to append to service name to provide alternate
-    configuration (or support multiple services)
+:param service_name: name of the service to stop.
+
 """)
 
 restart = __proxy.method('restart', True, doc=
 """
 Restart process.
 
-:param name_prefix: Prefix to append to service name to provide alternate
-    configuration (or support multiple services)
+:param service_name: name of the service to restart.
+
 """)
+
+status = __proxy.method('status', True, doc=
+"""
+Process status.
+
+:param service_name: name of the service to get status of.
+
+""")
+
+@__proxy.local_method
+def install_config(name_prefix=None):
+    """
+    Install configuration.
+
+    :param name_prefix: name prefix for configuration.
+    """
+    upload_config(name_prefix)
+    manager_reload()
 
 __all__ = __proxy.methods
--- denim/service/supervisor.py
+++ denim/service/supervisor.py
@@ -6,8 +6,87 @@ SERVICE_NAME = 'supervisor'
 
 
 def upload_config(name_prefix=None):
+    """
+    Upload configuration file.
+
+    :param name_prefix: Prefix to append to service name to provide alternate
+        configuration (or support multiple services)
+
+    """
     put(
         paths.local_config_file(SERVICE_NAME, name_prefix),
         paths.remote_config_file('/etc/supervisor/conf.d', name_prefix),
         use_sudo=True
     )
+
+
+def manager_start():
+    """
+    Start service manager.
+
+    """
+    sudo('/etc/init.d/supervisor start')
+
+
+def manager_stop():
+    """
+    Stop service manager.
+
+    """
+    sudo('/etc/init.d/supervisor stop')
+
+
+def manager_restart():
+    """
+    Restart service manager.
+
+    """
+    sudo('/etc/init.d/supervisor restart')
+
+
+def manager_reload():
+    """
+    Reload service manager.
+
+    """
+    sudo('supervisorctl reread')
+
+
+def start(service_name):
+    """
+    Start process.
+
+    :param service_name: name of the service to start in supervisor.
+
+    """
+    sudo('supervisorctl start %s' % service_name)
+
+
+def stop(service_name):
+    """
+    Stop process.
+
+    :param service_name: name of the service to stop in supervisor.
+
+    """
+    sudo('supervisorctl start %s' % service_name)
+
+
+def restart(service_name):
+    """
+    Restart process.
+
+    :param service_name: name of the service to restart in supervisor.
+
+    """
+    sudo('supervisorctl restart %s' % service_name)
+
+
+def status(service_name):
+    """
+    Process status.
+
+    :param service_name: name of the service to get status of.
+
+    """
+    sudo('supervisorctl status %s' % service_name)
