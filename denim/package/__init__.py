e46274d76172492d48de2ad5148f38c8583537db
Tim Savage <tim.savage@poweredbypenguins.org>
2012-07-06 13:29:15 +1000

    Added package management support, includes support for Debian based systems

---
+++ denim/package/__init__.py
@@ -0,0 +1,35 @@
+# -*- encoding:utf8 -*-
+"""
+Methods for managing OS packages.
+
+An alternative package manager can be selected by settings the:
+``deploy_package_manager`` env variable.
+
+Note: Different distributions/operating systems have different naming
+conventions for packages. For example Apache is referred to as apache on
+Debian systems but httpd on Redhat systems.
+
+Options:
+- debian (default) Debian package management tools (apt, dpkg etc)
+
+"""
+from denim._env_proxy import Proxy
+
+
+__proxy = Proxy('deploy_package_manager', globals(), 'debian')
+
+is_installed = __proxy.method('is_installed', False, doc=
+"""
+Check if a particular package has been installed.
+
+:param name: name of the package to check for.
+""")
+
+install = __proxy.method('install', False, doc=
+"""
+Install a package.
+
+:param name: name of the package to install.
+""")
+
+__all__ = __proxy.methods
---
+++ denim/package/debian.py
@@ -0,0 +1,21 @@
+from fabric.api import sudo
+from denim import utils
+
+
+def is_installed(name):
+    """
+    Check if a particular package has been installed.
+
+    :param name: name of the package to check for.
+    """
+    result = utils.run_test('dpkg --status "%s" | grep Status' % name)
+    return result.succeeded
+
+
+def install(name):
+    """
+    Install a package.
+
+    :param name: name of the package to install.
+    """
+    sudo('apt-get install "%s"' % name)
