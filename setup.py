fb4265ca2aaa2e5f59d198691d774931df9c5d68
Tim Savage <tim.savage@poweredbypenguins.org>
2013-04-09 15:46:24 +1000

    Updated setup file to include correct package data etc General cleanup of setup

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 import denim
+
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
@@ -8,27 +9,24 @@ def read(fname):
 setup(
     name='denim',
     version=denim.__version__,
-    url='http://bitbucket.org/timsavage/denim',
+    packages= find_packages(exclude=('tests', 'tests.*')),
+    scripts=['bin/den'],
+
+    install_requires=['fabric'],
+
+    package_data={
+        'denim.scaffold': ['templates/*.txt', 'templates/*/*.txt'],
+    },
+
+    # Metadata for PyPI
     author='Tim Savage',
     author_email='tim.savage@poweredbypenguins.org',
     description='A Fabric deployment strategy for Python web applications.',
-#    long_description=read('README.rst'),
-    packages=[
-        'denim',
-        'denim.django',
-        'denim.package',
-        'denim.scaffold',
-        'denim.scm',
-        'denim.service',
-        'denim.webserver',
-    ],
-# Enable this once scaffolding support is more mature.
-#    scripts=[
-#        'bin/den',
-#    ],
-    requires=['fabric'],
+    license='BSD',
+    url='http://bitbucket.org/timsavage/denim',
     classifiers=[
         'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
     ]
 )
