cc63b8bef0f7d3d7723431e88792dc6d39624c8b
Tim Savage <tim.savage@poweredbypenguins.org>
2014-03-03 01:16:57 +1100

    Initial work for class based recipes

---
+++ denim/recipies/base.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+
+class RecipeBase(object):
+    """
+    Base class for recipes.
+    """
+    pass
---
+++ denim/recipies/django.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from denim.recipies.base import RecipeBase
+
+
+class Setup(RecipeBase):
+    """
+    Standard setup recipe for Django applications.
+    """
+    pass
+
+
+class Deploy(RecipeBase):
+    """
+    Standard deployment recipe for Django applications.
+    """
+    pass
