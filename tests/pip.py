09da2632aaece62aa31621852a4da61301781517
Tim Savage <tim.savage@poweredbypenguins.org>
2014-03-04 00:35:56 +1100

    Fixed pip test to use new current release path

--- tests/pip.py
+++ tests/pip.py
@@ -8,8 +8,7 @@ class TestPip(ApiTestCase):
     def test_pip_install_requirements_default(self):
         pip.install_requirements()
 
-        self.assertSudo('pip install --upgrade -r /opt/test-project/app/current/requirements.txt',
-            user=RootUser)
+        self.assertSudo('pip install --upgrade -r /opt/test-project/current/requirements.txt', user=RootUser)
 
     def test_pip_install_requirements_no_upgrade_specified_path(self):
         pip.install_requirements(upgrade=False, path_to_requirements='/tmp/requirements.txt')
